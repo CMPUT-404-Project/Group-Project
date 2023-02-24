a62fa1dd7c7db0741c9617213b500fe1614c99bc
AzureAD\HarshPatel <harsh.patel@mobiuslogic.com>
2023-02-23 23:31:42 -0700

    added Comment

--- socialdistribution/home/models.py
+++ socialdistribution/home/models.py
@@ -9,12 +9,20 @@ class Author(models.Model):
     displayName = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     github = models.CharField(max_length=200)
+    #added this -Harsh
     profile_image = models.URLField(max_length=200, blank=True,null=True,default="")
 
+    #returns id of the author
+    def get_id(self):
+        return self.id
+    
     def __str__(self):
-        return self.displayName
+        return str(self.displayName) + '-' + str(self.id)
+    
+
     
 class Post(models.Model):
+
      # To use choices I followed this: https://www.geeksforgeeks.org/how-to-use-django-field-choices 
     CONTENT_TYPE = (
         ('text/markdown', 'text/markdown'),
@@ -39,27 +47,56 @@ class Post(models.Model):
     #caption of the post
     caption = models.CharField(max_length=300,blank=True,null=True)
     #author of the post
-    author = models.ForeignKey(Author)
+    #the on_delete = models.CASCADE deletes all the foreignkey pointing to author from the AUTHOR object
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posted')
+    #number of likes
+    count_likes = models.IntegerField(default=0)
     #time post was published
     published_time = models.DateTimeField('date published',default=now)
     #visibility of the post
     visibility = models.CharField(choices=VISIBILITY, default='PUBLIC')
     
     def __str__(self):
-        return self.title
+        return self.title + "(" + str(self.id) + ")"
 
+    #returns id of the post
     def get_id(self):
         return self.id
+    
+
 
 class Comment(models.Model):
+
     CONTENT_TYPE = (
         ('text/markdown', 'text/markdown'),
         ('text/plain','text/plain')
     )
 
+    #id, the primary key
+    id = models.AutoField(primary_key=True)
+    #post where comment posted
+    post = models.ForeignKey(Post,related_name='comments')
+    #author of the comment
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    #content type of the comment
+    content_type = models.CharField(max_length=150,choices=CONTENT_TYPE,default='text/plain')
+    #comment on post
+    comment = models.TextField()
+    #date comment was published
+    published_date = models.DateTimeField('date published', default=now)
+    
+
+
 
 class Like(models.Model):
-    pass
+    #id, the primary key
+    id = models.AutoField(primary_key=True)
+    #post where comment posted
+    post = models.ForeignKey(Post,related_name='comments')
+    #author of the comment
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    #date comment was published
+    published_date = models.DateTimeField('date published', default=now)
 
 class Inbox(models.Model):
     pass
