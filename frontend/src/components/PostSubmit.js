a37ffcbbac85a73971b13bc5983fa244bf68266c
naviza <43558584+naviza@users.noreply.github.com>
2023-02-27 22:19:58 -0700

    Add some more frontend

---
+++ frontend/src/components/PostSubmit.js
@@ -0,0 +1,63 @@
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+
+function PostSubmit(props) {
+    const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Create Post
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group className="mb-3" controlId="postTitle">
+                    <Form.Label>Post Title</Form.Label>
+                    <Form.Control type="text" placeholder="Title" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="postType">
+                    <Form.Select>
+                        <Form.Label>Post Type</Form.Label>
+                        <option>text/markdown</option>
+                        <option>text/plain</option>
+                        <option>application/base64</option>
+                        <option>image/png;base64</option>
+                        <option>image/jpeg;base64</option>
+                    </Form.Select>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="postContent">
+                    <Form.Label>Post Content</Form.Label>
+                    <Form.Control type="text" placeholder="content" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default PostSubmit;
