43b4e48190338ac304b119ecd038d7b53f092628
AzureAD\HarshPatel <harsh.patel@mobiuslogic.com>
2023-02-28 17:29:38 -0700

    connected back and front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,42 +6,27 @@ import Post from './components/Post';
 import PostSubmit from './components/PostSubmit';
 import Navigation from './components/Navigation';
 import {submitPost, likeData, getPosts} from './Logic';
+import { useEffect } from 'react';
+import axios from 'axios'; 
 
 
 
 function App() {
 
-  const postItems = getPosts().map((onePost) => 
-    <Post postObject={onePost} key={onePost.id} />
-  );
-
+  // const postItems = getPosts().map((onePost) => 
+  //   <Post postObject={onePost} key={onePost.id} />
+  // );
+  const postItems = {}
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log('hello')
+    axios.get('http://127.0.0.1:8000/authors').then(res => {console.log(res.data)})
+  },[]);
   return (
     <div className="App">
 
-      
+      HELLO
 
-      <div id="navbar-area">
-        <Navigation />
-      </div>
-      
-      <br /> <br />
-
-      <Container>
-
-      <div id="create-post">
-        <PostSubmit submitAction={submitPost}/>
-      </div>
-
-      <div id="posts-section">
-        {postItems}
-      </div>
-
-
-      <div id="footer-area">
-        {/* I guess this is empty for now? */}
-      </div>
-
-      </Container>
       
     </div>
   );
