a6a9f098f22bcac57d89246c2f3a4edddaa9148d
“Mashiad <mashiad@ualberta.ca>
2023-03-02 23:16:59 -0700

    Updated.

--- socialdistribution/authors/forms.py
+++ socialdistribution/authors/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import Author
-from .models import User
+from .models import CustomUser
 from django.contrib.auth.forms import AuthenticationForm
 
 class AuthorSignupForm(UserCreationForm):
@@ -11,7 +11,7 @@ class AuthorSignupForm(UserCreationForm):
     github = forms.CharField(max_length=200)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'password1', 'password2', 'host', 'displayName', 'url', 'github']
 
     def save(self, commit=True):
@@ -22,15 +22,14 @@ class AuthorSignupForm(UserCreationForm):
         github_data = self.cleaned_data['github']
         if commit:
             user.save()
-            author = Author.objects.create(user=user, host=host_data, displayName=displayName_data,url=url_data,github=github_data)
+            author = Author.objects.create(customuser=user, host=host_data, displayName=displayName_data,url=url_data,github=github_data)
             
         return author
 
 class UserLoginForm(AuthenticationForm):
     
-
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['username', 'password']
 
 
--- socialdistribution/authors/models.py
+++ socialdistribution/authors/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
 import uuid
-from django.contrib.auth.models import AbstractUser
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.hashers import check_password
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, username, password=None, **extra_fields):
@@ -18,16 +15,11 @@ class UserAccountManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(username, password, **extra_fields)
 
-class User(AbstractBaseUser):
+class CustomUser(AbstractBaseUser):
     username = models.CharField(max_length=50, unique=True)
     
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
-    # host = models.CharField(max_length=200)
-    # displayName = models.CharField(max_length=200)
-    # url = models.CharField(max_length=200)
-    # github = models.CharField(max_length=200)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
@@ -36,13 +28,11 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
-    
-
 
 # Create your models here.
 class Author(models.Model):
     # user_name = models.CharField(unique=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author', default=None)
+    customuser = models.OneToOneField('authors.CustomUser', on_delete=models.CASCADE, related_name='author', default=None)
     id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     type = models.CharField(max_length=20,default="author", editable=False)
     host = models.CharField(max_length=200)
