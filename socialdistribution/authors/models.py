365cbce80c37bd9f557f32eab7a23632da2cadc2
“Mashiad <mashiad@ualberta.ca>
2023-03-12 20:22:22 -0600

    Admin fixes.

--- socialdistribution/authors/models.py
+++ socialdistribution/authors/models.py
@@ -20,6 +20,7 @@ class CustomUser(AbstractBaseUser):
     
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
@@ -28,6 +29,15 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+    
+    def has_perm(self, perm, obj=None):
+        # Check if the user has the specified permission
+        return self.is_superuser
+    def has_module_perms(self, app_label):
+        # Check if the user has permissions 
+        if self.is_superuser:
+            return True
+        return False
 
 # Create your models here.
 class Author(models.Model):
