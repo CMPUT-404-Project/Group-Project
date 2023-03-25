b598c9289b2caf8dfa242f333f45ebf4b3d5c280
Richmond Naviza <naviza@ualberta.ca>
2023-03-25 10:03:43 -0600

    Revert "Update settings.py"
    This reverts commit b2569c230edc2d637654214433aeb6d48d7c74de.

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os, django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$_36yrktyy9k)66uqv1kx9gx7h2)2a^du_mp$55pjd1w(17hbg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['distributed-social-net.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -43,14 +43,12 @@ INSTALLED_APPS = [
     'authors',
     'posts',
     'inbox',
-    'django_heroku',
     'corsheaders', #for rest api
 ]
 
 MIDDLEWARE = [
     
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -98,11 +96,11 @@ WSGI_APPLICATION = 'socialdistribution.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd53ec0944ntllo',
-        'USER': 'tvnsjgcnkfvmze',
-        'PASSWORD': '366bb85c5db401039ce2f530e0ad9b9f267bbadf95e52d57806b1be3fb67fa09',
-        'HOST': 'ec2-34-197-91-131.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'NAME': 'db_socialdistribution',
+        'USER': 'globaluser',
+        'PASSWORD': 'globalpassword',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
@@ -144,12 +142,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-if not os.path.exists(STATIC_ROOT):
-    os.makedirs(STATIC_ROOT)
 STATIC_URL = 'static/'
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
