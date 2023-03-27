34fb240a5b56cc633df0ac4c4b10acf3dd0cc509
“Mashiad <mashiad@ualberta.ca>
2023-03-26 19:02:30 -0600

    profile_image renamed to profileImage

--- authors/models.py
+++ authors/models.py
@@ -76,7 +76,7 @@ class Author(models.Model):
     displayName = models.CharField(max_length=200,unique=True)
     url = models.CharField(max_length=200)
     github = models.CharField(max_length=200, null=True)
-    profile_image = models.ImageField(upload_to='profile_images',null=True,default='blank_profile.png')
+    profileImage = models.CharField(null=True,blank=True)
 
     #returns id of the author
     def get_id(self):
