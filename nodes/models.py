2059eed48c7048da176b1d5d76a212a16bd965db
“Mashiad <mashiad@ualberta.ca>
2023-03-29 21:26:23 -0600

    Update models.py

--- nodes/models.py
+++ nodes/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Node(models.Model):
     team_num= models.IntegerField(primary_key=True)
-    api_url = models.CharField(max_length=255, blank=True, null=True)
-    api_author_prefix = models.CharField(max_length=255, blank=True, null=True)
+    api_author_url = models.CharField(max_length=255, blank=True, null=True)
+    api_inbox_prefix = models.CharField(max_length=255, blank=True, null=True)
     auth_token = models.CharField(max_length=255, blank=True, null=True)
     username = models.CharField(max_length=255, blank=True, null=True)
     password = models.CharField(max_length=255, blank=True, null=True)
