adfd1a70e6f6990d0782860fb08b6c018af7802d
Richmond Naviza <naviza@ualberta.ca>
2023-04-02 11:29:23 -0600

    Add more checks to Follow Request

--- frontend/src/components/author_lookup/Following.js
+++ frontend/src/components/author_lookup/Following.js
@@ -1,16 +1,22 @@
 
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 import stubbed_followers from '../stubobject';
+import axios from 'axios';
 
 // These are the authors that You Follow
 function Following(props) {
-    const [authors_you_follow, set_authors_you_follow] = useState(stubbed_followers);
+    const [authors_you_follow, set_authors_you_follow] = useState([]);
+
+    useEffect(() => {
+        // axios.get(props.author.id, {headers:{Authorization:props.authString}}).then((resp) => set_authors_you_follow(resp.data.items));
+    }, []); // run this when it renders
+
     const remove_an_author = (hostname, authorID) => {
         // create a DELETE Request
         // create a GET request
@@ -18,7 +24,7 @@ function Following(props) {
         console.log("Unfollowing: ", authorID);
     }
 
-    const authors_you_follow_components = authors_you_follow.items.map((author) => {
+    const authors_you_follow_components = authors_you_follow.map((author) => {
         return (<ListGroup.Item key={author.id}>
             {author.displayName}
             <Button variant="outline-danger" onClick={() => remove_an_author(author.host, author.id)}>Unfollow</Button>
